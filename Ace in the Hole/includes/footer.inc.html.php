866cadaff38363fd61265eac6387bb8a320987f5
thetomosaurus <61666110+thetomosaurus@users.noreply.github.com>
2020-06-02 00:57:53 -0700

    Add files via upload

---
+++ Ace in the Hole/includes/footer.inc.html.php
@@ -0,0 +1,14 @@
+
+<footer>
+
+            <p id="designer">
+    Site design by Jamie Tomosunas, who is a student at <a href="http://www.pcc.edu" target="_blank">PCC</a>
+            </p>
+
+            <hr>
+
+            <p id="contact">Jamie Tomosunas<a href="https://tomosaurus.webhostingforstudents.com/" target="_blank">https://tomosaurus.webhostingforstudents.com/</a>
+            </p>
+    <p>Copyright &copy; <?php $year = date("Y"); echo $year; ?> Ace in the Hole <br>
+        <a href="mailto:hello@aceinthehole.com">hello@aceinthehole.com</a></p>
+</footer>
